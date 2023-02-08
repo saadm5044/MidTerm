22cac3c9a22a09648ddd71625fad0ac59b88c63c
Junaid Saad Ahmed <saadm5044@gmail.com>
2023-02-07 20:20:28 -0400

    Initial Commit

---
+++ routes/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET Home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home',});
+});
+
+/* GET Home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home',});
+});
+
+/* GET About Us page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About',});
+});
+
+/* GET Projects page. */
+router.get('/products', function(req, res, next) {
+  res.render('index', { title: 'Products',});
+});
+
+/* GET Services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services',});
+});
+
+/* GET Contact Us page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact',});
+});
+module.exports = router;
