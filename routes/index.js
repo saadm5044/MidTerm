51480640497737a9d210ba6b607f5948bc21f284
Junaid Saad Ahmed <saadm5044@gmail.com>
2023-02-08 00:09:43 -0400

    Created First Website

--- routes/index.js
+++ routes/index.js
@@ -3,31 +3,31 @@ var router = express.Router();
 
 /* GET Home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home',});
+  res.render('home', { title: 'Home',});
 });
 
 /* GET Home page. */
 router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Home',});
+  res.render('home', { title: 'Home',});
 });
 
 /* GET About Us page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About',});
+  res.render('about', { title: 'About',});
 });
 
 /* GET Projects page. */
-router.get('/products', function(req, res, next) {
-  res.render('index', { title: 'Products',});
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects',});
 });
 
 /* GET Services page. */
 router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services',});
+  res.render('services', { title: 'Services',});
 });
 
 /* GET Contact Us page. */
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact',});
+  res.render('contact', { title: 'Contact',});
 });
 module.exports = router;
